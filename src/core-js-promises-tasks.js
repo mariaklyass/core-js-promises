81763ed0bfa98dd386d271dfe508988270a8ada9
mariaklyass <klyassmaria@gmail.com>
2024-04-15 23:00:32 +0300

    feat: solve getPromise, getPromiseResult, getFirstResolvedPromiseResult,getFirstPromiseResult

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -17,8 +17,14 @@
  * 0    => promise that will be fulfilled
  * 1    => promise that will be fulfilled
  */
-function getPromise(/* number */) {
-  throw new Error('Not implemented');
+function getPromise(number) {
+  return new Promise((resolve, reject) => {
+    if (number >= 0) {
+      resolve(number);
+    } else {
+      reject(new Error('Number is negative'));
+    }
+  });
 }
 
 /**
@@ -33,8 +39,16 @@ function getPromise(/* number */) {
  * Promise.resolve('success') => promise that will be fulfilled with 'success' value
  * Promise.reject('fail')     => promise that will be fulfilled with 'fail' value
  */
-function getPromiseResult(/* source */) {
-  throw new Error('Not implemented');
+function getPromiseResult(source) {
+  return new Promise((resolve) => {
+    source
+      .then(() => {
+        resolve('success');
+      })
+      .catch(() => {
+        resolve('fail');
+      });
+  });
 }
 
 /**
@@ -50,8 +64,19 @@ function getPromiseResult(/* source */) {
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)]  => Promise fulfilled with 1
  * [Promise.reject(1), Promise.reject(2), Promise.reject(3)]    => Promise rejected
  */
-function getFirstResolvedPromiseResult(/* promises */) {
-  throw new Error('Not implemented');
+function getFirstResolvedPromiseResult(promises) {
+  return new Promise((resolve, reject) => {
+    promises.forEach((promise) => {
+      promise.then(
+        (value) => resolve(value),
+        (error) => reject(error)
+      );
+    });
+
+    setTimeout(() => {
+      reject(new Error('All promises were rejected'));
+    }, 0);
+  });
 }
 
 /**
@@ -73,8 +98,15 @@ function getFirstResolvedPromiseResult(/* promises */) {
  * [promise3, promise6, promise2] => Promise rejected with 2
  * [promise3, promise4, promise6] => Promise rejected with 6
  */
-function getFirstPromiseResult(/* promises */) {
-  throw new Error('Not implemented');
+function getFirstPromiseResult(promises) {
+  return new Promise((resolve, reject) => {
+    promises.forEach((promise) => {
+      promise.then(
+        (value) => resolve(value),
+        (error) => reject(error)
+      );
+    });
+  });
 }
 
 /**
